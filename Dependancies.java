43aa70c0c1c08986cd71ec6f75d9f55c3e7e6151
Divyesk Kevadiya <kevadiyadivyesh@gmail.com>
2021-10-19 15:18:42 +0530

    Update Dependancies.java

--- Dependancies.java
+++ Dependancies.java
@@ -30,8 +30,42 @@ implementation 'com.github.javiersantos:AppUpdater:2.7'
 implementation 'com.kaopiz:kprogresshud:1.2.0'
 implementation 'com.onesignal:OneSignal:3.14.0'
 implementation 'com.google.code.gson:gson:2.8.5'
-implementation 'com.squareup.retrofit2:converter-gson:2.5.0'
+
+implementation 'com.squareup.retrofit2:retrofit:2.9.0'
+implementation 'com.squareup.retrofit2:converter-scalars:2.9.0'
+implementation 'com.squareup.retrofit2:converter-gson:2.9.0'
+implementation 'com.squareup.okhttp3:okhttp:4.9.0'
+implementation 'com.squareup.okhttp3:logging-interceptor:4.9.0'
+ 
 implementation 'com.squareup.okhttp3:logging-interceptor:3.8.0'
+def room_version = "2.3.0"
+implementation "androidx.room:room-runtime:$room_version"
+annotationProcessor "androidx.room:room-compiler:$room_version"
+implementation 'com.github.chaya222:AndroidOtpView:0.0.2'
+implementation 'com.android.volley:volley:1.2.0'
+ 
+implementation 'com.google.firebase:firebase-auth:20.0.4'
+implementation 'com.google.firebase:firebase-database:19.7.0'
+implementation 'com.google.firebase:firebase-messaging:22.0.0' 
+ 
+implementation "androidx.core:core-ktx:+"
+implementation "org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlin_version"
+implementation "android.arch.persistence.room:runtime:1.1.1"
+kapt "android.arch.persistence.room:compiler:1.1.1" 
+ 
+implementation 'net.danlew:android.joda:2.9.9.4'
+implementation 'com.github.PhilJay:MPAndroidChart:v3.0.3'
+implementation 'com.facebook.android:facebook-login:4.34.0'
+implementation 'se.emilsjolander.stickylistheaders:library:2.1.0'
+implementation 'com.stripe:stripe-android:6.1.2'
+implementation 'com.budiyev.android:code-scanner:1.9.4'
+implementation 'com.github.mabbas007:TagsEditText:1.0.5'
+implementation 'com.chauthai.swipereveallayout:swipe-reveal-layout:1.4.1'
+implementation 'com.github.mabbas007:TagsEditText:1.0.5'
+implementation 'info.hoang8f:android-segmented:1.0.6'
+
+ 
+ 
 
 ----------------------------Scanner---------------------------
 implementation 'me.dm7.barcodescanner:zbar:1.9.3'
@@ -73,6 +107,28 @@ buildTypes {
                 }
             }
         }
+    }
+
+-----------------------Signing Config-----------------------
+signingConfigs {
+        config {
+            keyAlias 'EvaKey'
+            keyPassword 'avdevs@123'
+            storeFile file('E:\\dump\\eva-android\\eva-keystore.jks')
+            storePassword 'avdevs@123'
+        }
+        configCollabera {
+            keyAlias 'collaberakey'
+            keyPassword '0utofmind'
+            storeFile file('E:\\dump\\eva-android\\collabera-keystore.jks')
+            storePassword '0utofmind'
+        }
+    }
+
+
+     buildFeatures {
+        dataBinding true
+        viewBinding true
     }
 
 
